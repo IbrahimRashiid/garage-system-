5e5116d898becb06e5f2e07db3c738188c9a804b
Ibrahim Rashid <104211987+IbrahimRashiid@users.noreply.github.com>
2023-04-26 11:24:15 +0200

    Delete Garage-system directory

--- Garage-system/SlotConfiguration.java
+++
@@ -1,9 +0,0 @@
-package SW_assignment;
-
-import java.util.ArrayList;
-
-//SuperClass
-interface SlotConfiguration {
-    //Overriden function
-    public int park(Vehicle car, ArrayList<SlotController> garage, int numOfSlots);
-}
